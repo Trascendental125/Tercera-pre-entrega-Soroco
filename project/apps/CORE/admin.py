84b86917bde3a7c1f58d2024172106ec0e6edced
Trascendental125 <asoroco@gmail.com>
2023-07-13 01:57:20 -0300

    Modelos creados y registrados OK

--- project/apps/CORE/admin.py
+++ project/apps/CORE/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models 
+
+admin.site.register(models.Asegurado)
+admin.site.register(models.Aseguradora)
+admin.site.register(models.Poliza)
--- project/apps/CORE/models.py
+++ project/apps/CORE/models.py
@@ -19,11 +19,10 @@ class Aseguradora(models.Model):
 
 
 class Poliza(models.Model):
-    asegurado = models.ForeignKey(Asegurado, on_delete=models.CASCADE)
-    aseguradora = models.ForeignKey(Aseguradora, on_delete=models.CASCADE)
-    numero_poliza = models.CharField(max_length=50)
-    seccion = models.CharField(max_length=100)
-    # Otros campos necesarios para la póliza
+    asegurado = models.ForeignKey(Asegurado, on_delete=models.SET_NULL, null=True, blank=True)
+    aseguradora = models.ForeignKey(Aseguradora, on_delete=models.SET_NULL, null=True, blank=True)
+    seccion = models.CharField(max_length=100, blank=True)
+    numero_poliza = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.numero_poliza
